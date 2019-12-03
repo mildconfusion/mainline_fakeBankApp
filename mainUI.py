31872ce3c7c456370a941325bc20c1a79a6ab8cb
mildconfusion <52550976+mildconfusion@users.noreply.github.com>
2019-12-03 13:53:41 -0800

    fixed a little typo, bud

--- mainUI.py
+++ mainUI.py
@@ -1,4 +1,4 @@
-uName = input("Hello There Bud, Welcome to the Bank App.\n""Please enter your username:"
+uName = input("Hello There Bud, Welcome to the Bank App.\n""Please enter your username:")
 
 print("You entered:",uName)
 
