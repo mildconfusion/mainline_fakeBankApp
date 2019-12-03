77e31998283cce61d7f2e75e1845353a223c8a65
mildconfusion <52550976+mildconfusion@users.noreply.github.com>
2019-12-03 13:50:18 -0800

    put in the part about how hamsters run this show

--- mainUI.py
+++ mainUI.py
@@ -1,3 +1,6 @@
 uName = input("Hello There Bud, Welcome to the Bank App.\n""Please enter your username:"
 
 print("You entered:",uName)
+
+print("All right bud, we gawn move forward with this whole banking app business meow, so hold on tight!"
+print("This application is powered by little hamsters that turn the wheels by running rapidly.")
