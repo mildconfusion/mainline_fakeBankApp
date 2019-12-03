61fcc2046a86e14266845853ecaf6095991ae21a
mildconfusion <52550976+mildconfusion@users.noreply.github.com>
2019-12-03 13:56:54 -0800

    fixed syntax and added user prompt for PIN

--- mainUI.py
+++ mainUI.py
@@ -1,6 +1,7 @@
 uName = input("Hello There Bud, Welcome to the Bank App.\n""Please enter your username:")
+uPIN = input("Put in your PIN bud:")
 
 print("You entered:",uName)
 
-print("All right bud, we gawn move forward with this whole banking app business meow, so hold on tight!"
+print("All right bud, we gawn move forward with this whole banking app business meow, so hold on tight!")
 print("This application is powered by little hamsters that turn the wheels by running rapidly.")
